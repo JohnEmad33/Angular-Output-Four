68c15eeefcad1b025a5dac7e9e8946f7d676de9b
John Emad Morcos Sadek | Ejada Systems Ltd <jemad@ejadacairo.local>
2023-01-28 23:37:11 +0200

    create reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularOutputFour';
+  reactiveForm: FormGroup;
+
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl(null, Validators.required),
+      lastname: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required),
+      confirmPassword: new FormControl(null, Validators.required),
+      age: new FormControl(null, Validators.required),
+      gender: new FormControl('male', Validators.required),
+      skills: new FormArray([new FormControl(null, Validators.required)]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.reactiveForm);
+  }
+
+  addSkills() {
+    (<FormArray>this.reactiveForm.get('skills')).push(
+      new FormControl(null, Validators.required)
+    );
+  }
 }
